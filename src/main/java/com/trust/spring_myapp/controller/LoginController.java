e4d946e98b478bb8cf0916aeed5b56bbcccae6ff
suu2626 <154961142+suu2626@users.noreply.github.com>
2024-06-19 15:36:44 +0900

    Update LoginController.java

--- src/main/java/com/trust/spring_myapp/controller/LoginController.java
+++ src/main/java/com/trust/spring_myapp/controller/LoginController.java
@@ -5,25 +5,25 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller // このクラスがWebコントローラーであることを示します
+@Controller // このクラスがWebコントローラーであることを示す
 public class LoginController {
 
-    @GetMapping("/login") // "/login"というURLに対するGETリクエストを処理します
+    @GetMapping("/login") // "/login"というURLに対するGETリクエストを処理
     public String login() {
-        return "login";  // login.htmlを表示します
+        return "login";  // login.htmlを表示
     }
     
-    @GetMapping("/") // ルートURL ("/") に対するGETリクエストを処理します
+    @GetMapping("/") // ルートURL ("/") に対するGETリクエストを処理
     public String redirectToIndex() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication(); // 現在のユーザーの認証情報を取得します
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication(); // 現在のユーザーの認証情報を取得
         if (authentication != null && authentication.isAuthenticated()) { // ユーザーがログインしている場合
-            return "redirect:/index";  // "/index"にリダイレクトします
+            return "redirect:/index";  // "/index"にリダイレクト
         }
-        return "redirect:/login"; // ユーザーがログインしていない場合、"/login"にリダイレクトします
+        return "redirect:/login"; // ユーザーがログインしていない場合、"/login"にリダイレクト
     }
     
-    @GetMapping("/index") // "/index"というURLに対するGETリクエストを処理します
+    @GetMapping("/index") // "/index"というURLに対するGETリクエストを処理
     public String index() {
-        return "index"; // index.htmlを表示します
+        return "index"; // index.htmlを表示
     }
 }
